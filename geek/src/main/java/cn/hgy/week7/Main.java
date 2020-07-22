861358e21575b39d8225108aed343a130b9e7b1f
huanggy <17759217302@qq.com>
2020-07-22 09:56:48 +0800

    feat: 优化测试数据（将域名改成ip，避免DNS解析耗时）

--- geek/src/main/java/cn/hgy/week7/Main.java
+++ geek/src/main/java/cn/hgy/week7/Main.java
@@ -10,14 +10,17 @@ public class Main {
 
     public static void main(String[] args) {
         long begin = System.currentTimeMillis();
-        TestResult testResult = TestHelper.test("http://www.baidu.com", 100,10);
-        System.out.println(testResult.toString());
-
-        testResult.getResponseTimeList().stream().sorted().collect(Collectors.toList()).forEach(l->{
+        // 将域名解析成IP
+        TestResult testResult = TestHelper.test("http://14.215.177.39", 100, 10);
+        System.out.println("测试结果为：" + testResult.toString());
+        System.out.println("按顺序执行响应结果如下：");
+        testResult.getResponseTimeList().forEach(l -> {
             System.out.print(l + ",");
         });
-        long end = System.currentTimeMillis();
         System.out.println();
-        System.out.println("总耗时：" + (end - begin));
+        System.out.println("按响应顺序排序之后结果如下：");
+        testResult.getResponseTimeList().stream().sorted().forEach(l -> {
+            System.out.print(l + ",");
+        });
     }
 }
--- geek/src/main/java/cn/hgy/week7/TestHelper.java
+++ geek/src/main/java/cn/hgy/week7/TestHelper.java
@@ -31,21 +31,29 @@ public class TestHelper {
      */
     public static TestResult test(String url, int totalRequestCount, int concurrencyCount) {
 
+        // 等待所有线程都执行完之后，才返回
         CountDownLatch countDownLatch = new CountDownLatch(totalRequestCount);
-
+        // 分配线程，核心线程数为最大并发数，等待队列为请求总数
         ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(concurrencyCount, concurrencyCount, 0, TimeUnit.SECONDS, new LinkedBlockingDeque<>(totalRequestCount));
 
+        // 测试结果
         TestResult result = new TestResult();
         result.setUrl(url);
 
+        // 添加都线程队列
         int count = totalRequestCount;
 
+        System.out.println("开始执行测测试用例。。。。。。");
+        long allBegin = System.currentTimeMillis();
+
         while (count > 0) {
-            HttpClient client = HttpClientBuilder.create().build();
+
             threadPoolExecutor.submit(() -> {
                 try {
+                    HttpClient client = HttpClientBuilder.create().build();
+                    HttpGet httpGet = new HttpGet(url);
                     long begin = System.currentTimeMillis();
-                    HttpResponse response = client.execute(new HttpGet(url));
+                    HttpResponse response = client.execute(httpGet);
                     if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
                         result.addResponse(Long.valueOf(System.currentTimeMillis() - begin).intValue());
                     } else {
@@ -65,6 +73,7 @@ public class TestHelper {
             e.printStackTrace();
         }
 
+        System.out.println("执行测测试用例结束。。。。。。耗时：" + (System.currentTimeMillis() - allBegin));
         return result;
     }
 }
